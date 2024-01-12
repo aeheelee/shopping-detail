2994f966196379f149c4752b5691ccb0cf6220c2
Dahye Han <139421887+dahyehan22@users.noreply.github.com>
2024-01-12 18:17:29 +0900

    style : 검색 페이지 레이아웃

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -45,20 +45,36 @@ export default function SearchPage() {
 }
 
 const StyledWrap = styled.div`
+  position: relative;
+  flex: 1;
   display: flex;
   max-width: 1240px;
-  height: calc(100% - 80px);
+  height: 100%;
   margin: 0 auto;
   align-items: flex-start;
   gap: 0 20px;
+  z-index: 1;
 `;
 
 const StyledFilter = styled.section`
   display: flex;
   width: 300px;
   height: 100%;
+  padding: 20px 0;
   flex-direction: column;
   justify-content: space-between;
+  box-sizing: border-box;
+
+  @media only screen and (max-width: 900px) {
+    display: none;
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 80%;
+    background-color: #fff;
+    z-index: 10;
+  }
 `;
 
 const StyledContent = styled.section`
