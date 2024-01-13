aa7b9d966f136f1af63b1591fe6c6bb0555fd290
김명희/회원서비스팀 <myeonghui.kim@musinsa.com>
2024-01-13 16:32:40 +0900

    초기 랜더링 시, 각 필터가 전체가 선택되어 있도록 수정

--- src/components/filter/SearchFilter.tsx
+++ src/components/filter/SearchFilter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 interface IData {
@@ -17,6 +17,7 @@ interface IProps {
 
 const SearchFilter = ({ filter }: IProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
   const handleClick = () => {
     setIsOpen((prev) => !prev);
   };
@@ -24,19 +25,32 @@ const SearchFilter = ({ filter }: IProps) => {
   //필터 데이터
   const filterData = filter.data;
 
+  useEffect(() => {
+    // 첫 번째 아이템의 id를 초기 선택 값으로 설정
+    if (filter.data.length > 0) {
+      setSelectedItemId(filter.data[0].id);
+    }
+  }, [filter.data]);
+
   return (
     <>
       <StyledFilterItem.Wrap>
         <StyledFilterItem.ButtonTitle type="button" onClick={handleClick}>
           {filter.title}
-          <StyledFilterItem.ButtonIcon isOpen={isOpen}>
+          <StyledFilterItem.ButtonIcon $isOpen={isOpen}>
             {isOpen ? '필터 선택 리스트 열림' : '필터 선택 리스트 닫힘'}
           </StyledFilterItem.ButtonIcon>
         </StyledFilterItem.ButtonTitle>
-        <StyledFilterItem.DetailList isOpen={isOpen}>
+        <StyledFilterItem.DetailList $isOpen={isOpen}>
           {filterData.map((item, index) => (
             <StyledFilterItem.DetailListItem key={index}>
-              <input type="radio" id={`${filter.type}_${item.id}`} name={filter.type} />
+              <input
+                type="radio"
+                id={`${filter.type}_${item.id}`}
+                name={filter.type}
+                checked={item.id === selectedItemId}
+                onChange={() => setSelectedItemId(item.id)}
+              />
               <StyledFilterItem.Label htmlFor={`${filter.type}_${item.id}`}>{item.title}</StyledFilterItem.Label>
             </StyledFilterItem.DetailListItem>
           ))}
@@ -58,7 +72,7 @@ const StyledFilterItem = {
     width: 100%;
     color: purple;
   `,
-  ButtonIcon: styled.i<{ isOpen: boolean }>`
+  ButtonIcon: styled.i<{ $isOpen: boolean }>`
     position: relative;
     display: block;
     width: 30px;
@@ -74,15 +88,15 @@ const StyledFilterItem = {
       width: 10px;
       height: 10px;
       -webkit-transform: translate(-50%, -50%) rotate(45deg);
-      transform: ${({ isOpen }) =>
-        isOpen ? 'translate(-50%, -50%) rotate(-135deg)' : 'translate(-50%, -50%) rotate(45deg)'};
+      transform: ${({ $isOpen }) =>
+        $isOpen ? 'translate(-50%, -50%) rotate(-135deg)' : 'translate(-50%, -50%) rotate(45deg)'};
       margin-top: 2px;
       border-top: 1px solid #333;
       border-left: 1px solid #333;
     }
   `,
-  DetailList: styled.ul<{ isOpen: boolean }>`
-    display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+  DetailList: styled.ul<{ $isOpen: boolean }>`
+    display: ${({ $isOpen }) => ($isOpen ? 'block' : 'none')};
     padding-top: 10px;
   `,
   DetailListItem: styled.li`
