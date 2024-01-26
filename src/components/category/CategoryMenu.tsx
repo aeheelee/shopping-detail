b666cb2a34ae7fc460dd54c0d1559140e0152ef6
한다혜/글로벌개발팀 <139421887+dahyehan22@users.noreply.github.com>
2024-01-26 17:14:29 +0900

    fix : 카테고리 메뉴 리렌더 제거

--- src/components/category/CategoryMenu.tsx
+++ src/components/category/CategoryMenu.tsx
@@ -1,22 +1,29 @@
 import styled from 'styled-components';
 import CategoryMenuItem from './CategoryMenuItem';
 import { useCategories } from '../../hooks/api/Categories';
+import { useEffect, useState } from 'react';
+import { ProductCategoryType } from '../../types/CommonTypes';
 
 const CategoryMenu = () => {
+  const [categories, setCategories] = useState<ProductCategoryType[]>([]);
   const { data } = useCategories();
 
-  if (!data) return null;
+  useEffect(() => {
+    if (data !== undefined) {
+      setCategories([
+        {
+          id: 0,
+          category: '',
+          title: '전체',
+          imageUrl:
+            'https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?auto=format&fit=crop&q=80&w=1740&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+        ...data.product,
+      ]);
+    }
+  }, [data]);
 
-  const categories = [
-    {
-      id: 0,
-      category: '',
-      title: '전체',
-      imageUrl:
-        'https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?auto=format&fit=crop&q=80&w=1740&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    ...data.product,
-  ];
+  if (!data) return null;
 
   return (
     <StyledWrap>
