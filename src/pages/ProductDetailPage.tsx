ffab45a6a65ba5e6e429b320ea7cae680908556e
이애희/서치앤디스커버리개발팀 <ah.lee@musinsa.com>
2024-01-27 10:24:33 +0900

    feat: 상품상세 전체 레이아웃 설정

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import styled from 'styled-components';
+import ReviewList from '../components/detail/review/ReviewList';
 
 const ProductDetailPage = () => {
-  return <div>상세 페이지입니다.</div>;
+  return (
+    <StyledWrap>
+      <ReviewList />
+    </StyledWrap>
+  );
 };
+
+const StyledWrap = styled.section`
+  min-width: 900px;
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 40px 50px 0;
+`;
 
 export default ProductDetailPage;
