a683893009ca8d6a740d0b4a345cc3ebd1ee27f7
김명희/회원서비스팀 <myeonghui.kim@musinsa.com>
2024-01-27 11:44:41 +0900

    fix: 상세페이지에 추천상품 컴포넌트 추가

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 import ReviewList from '../components/detail/review/ReviewList';
 import ProductDetail from '../components/detail/ProductDetail';
+import Recommend from '../components/detail/Recommend';
 
 const ProductDetailPage = () => {
   return (
     <StyledWrap>
       <ProductDetail />
       <ReviewList />
+      <Recommend />
     </StyledWrap>
   );
 };
@@ -15,7 +17,7 @@ const StyledWrap = styled.section`
   min-width: 900px;
   max-width: 1300px;
   margin: 0 auto;
-  padding: 40px 50px 0;
+  padding: 40px 50px;
 `;
 
 export default ProductDetailPage;
