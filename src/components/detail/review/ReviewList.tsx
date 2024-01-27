71063e27f933e04933fc520cb89215799e540e50
이애희/서치앤디스커버리개발팀 <ah.lee@musinsa.com>
2024-01-27 11:47:09 +0900

    feat: review 섹션 데이터 연동하기

--- src/components/detail/review/ReviewList.tsx
+++ src/components/detail/review/ReviewList.tsx
@@ -1,21 +1,44 @@
 import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+import { NumberParam, useQueryParams, withDefault } from 'use-query-params';
 import ReviewItem from './ReviewItem';
 import StarRate from './ReviewStarRate';
-// import Pagination from '../../Pagination';
+import Pagination from '../../Pagination';
+import useReview from '../../../hooks/api/useReview';
 
-const ReviewList = () => (
-  <StyledWrap>
-    <StyledText>90개 후기</StyledText>
-    <StyledList>
-      {/* 여기서 map */}
-      <StyledItems>
-        <ReviewItem />
-        <StarRate />
-      </StyledItems>
-    </StyledList>
-    {/* <Pagination maxPage={products.maxPage} currentPage={products.currentPage} onPageChange={handleChangePage} /> */}
-  </StyledWrap>
-);
+const ReviewList = () => {
+  const { productId } = useParams();
+  const [query, setQuery] = useQueryParams({
+    page: withDefault(NumberParam, 1),
+  });
+  const { data: reviewData } = useReview(query.page, Number(productId));
+
+  if (!reviewData) return null;
+
+  const handleChangePage = (page: number) => {
+    setQuery({ page });
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <StyledWrap>
+      <StyledText>90개 후기</StyledText>
+      <StyledList>
+        {reviewData.items.map((item, index) => {
+          const { rating, writer, content } = item;
+
+          return (
+            <StyledItems key={`review_${index}`}>
+              <ReviewItem writer={writer} content={content} />
+              <StarRate rating={rating} />
+            </StyledItems>
+          );
+        })}
+      </StyledList>
+      <Pagination maxPage={reviewData.maxPage} currentPage={query.page} onPageChange={handleChangePage} />
+    </StyledWrap>
+  );
+};
 
 const StyledWrap = styled.section``;
 const StyledList = styled.ul``;
