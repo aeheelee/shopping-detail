eefda22c070a495be163a65c1c9012e26e02ea44
이애희/서치앤디스커버리개발팀 <ah.lee@musinsa.com>
2024-01-27 15:00:58 +0900

    feat: suspense 추가

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
+import { Suspense } from 'react';
 import ReviewList from '../components/detail/review/ReviewList';
 import ProductDetail from '../components/detail/ProductDetail';
 import Recommend from '../components/detail/Recommend';
+import LoadingIndicator from '../components/LoadingIndicator';
 
 const ProductDetailPage = () => {
   return (
     <StyledWrap>
       <ProductDetail />
-      <ReviewList />
+      <Suspense fallback={<LoadingIndicator />}>
+        <ReviewList />
+      </Suspense>
       <Recommend />
     </StyledWrap>
   );
