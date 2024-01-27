b380f8509c2aed61830b3ff304c80813fae23241
김명희/회원서비스팀 <myeonghui.kim@musinsa.com>
2024-01-27 11:28:00 +0900

    feat: 상세페이지 ui 추가

---
+++ src/constants/productDetail.mockup.ts
@@ -0,0 +1,33 @@
+export const detailData = {
+  id: 3,
+  category: {
+    id: 1,
+    category: 'Clothing',
+  },
+  like: 908801,
+  info: '<h1>남성 가을 점퍼 사파리 바람막이점퍼</h1> <p>우아한 무드를 한층 높여주는 제품입니다.</p> <p>특별한 날, 더욱 빛나게 해줄 제품입니다.</p> <p>퀄리티 높은 소재 선택으로 오랜 시간 변하지 않는 품질을 자랑합니다.</p> <p>다양한 사이즈 옵션으로 모든 체형에 맞는 착용감을 제공합니다.</p> <p>베이직한 디자인에 특별한 디테일이 더해진 제품입니다.</p> <p>탄탄한 재봉 처리로 오래도록 사용 가능합니다.</p> <p>멋스러운 무드를 연출해주는 제품입니다.</p>',
+  purchaseStatus: {
+    totalSales: 918298,
+    satisfaction: '4.6',
+    age: {
+      type1: 15,
+      type2: 10,
+      type3: 45,
+      type4: 27,
+      type5: 3,
+    },
+    gender: {
+      man: 58,
+      woman: 42,
+    },
+  },
+  title: '남성 가을 점퍼 사파리 바람막이점퍼',
+  subTitle: null,
+  brandName: null,
+  price: 39000,
+  discountPercentage: 25,
+  discountPrice: 29250,
+  imageUrl:
+    'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1590&q=80',
+  brandImageUrl: null,
+};
